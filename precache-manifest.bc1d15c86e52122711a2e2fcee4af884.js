7d119000739d485870865cafe17bacd97161ec51
HappyJoseph <cassita0623@gmail.com>
2020-08-13 03:35:21 +0900

    commit portpolio

---
+++ precache-manifest.bc1d15c86e52122711a2e2fcee4af884.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b8e4e847be49b2e261c1774fc0bc2c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79fa1b318e4a1d1d821",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca3c0caba0bd9da0800b",
+    "url": "/static/js/2.6c36fe04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6c36fe04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e79fa1b318e4a1d1d821",
+    "url": "/static/js/main.c6723d56.chunk.js"
+  },
+  {
+    "revision": "f2c4288f27a794428a33",
+    "url": "/static/js/runtime-main.65856e23.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
